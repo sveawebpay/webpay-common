ef203e0404239ad1f52a6682a9f65f200399ba6c
Oliver Norin <oliver@notima.se>
2020-11-04 11:44:59 +0100

    changed date format

--- webpay-common/src/main/java/com/svea/webpay/common/conv/JsonUtil.java
+++ webpay-common/src/main/java/com/svea/webpay/common/conv/JsonUtil.java
@@ -1,11 +1,17 @@
 package com.svea.webpay.common.conv;
 
+import java.lang.reflect.Type;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonPrimitive;
+import com.google.gson.JsonSerializationContext;
+import com.google.gson.JsonSerializer;
 import com.svea.webpay.common.reconciliation.PaymentReport;
 
 /**
@@ -25,6 +31,17 @@ public class JsonUtil {
 		GsonBuilder builder = new GsonBuilder().setPrettyPrinting().setDateFormat(dfmtStr);
 		builder.setFieldNamingPolicy(FieldNamingPolicy.IDENTITY);
 		builder.setLenient();
+		builder.registerTypeAdapter(Date.class, new JsonSerializer<Date>() {
+
+			@Override
+			public JsonElement serialize(Date date, Type arg1, JsonSerializationContext arg2) {
+				if (date == null)
+					return null;
+				else
+					return new JsonPrimitive(new SimpleDateFormat(dfmtStr).format(date));
+			}
+			
+		});
 		gson = builder.create();
 		dfmt = new SimpleDateFormat(dfmtStr);
 		
