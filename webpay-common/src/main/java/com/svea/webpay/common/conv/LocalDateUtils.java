1b06e2538fbb92e954fb1ec767c8d019685964ec
Daniel Tamm <daniel@notima.se>
2020-11-18 16:46:55 +0100

    Null-safe LocalDateUtils.

--- webpay-common/src/main/java/com/svea/webpay/common/conv/LocalDateUtils.java
+++ webpay-common/src/main/java/com/svea/webpay/common/conv/LocalDateUtils.java
@@ -18,18 +18,22 @@ import java.util.Date;
 public class LocalDateUtils {
  
     public static Date asDate(LocalDate localDate) {
+    	if (localDate==null) return null;
         return Date.from(localDate.atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
     }
  
     public static Date asDate(LocalDateTime localDateTime) {
+    	if (localDateTime==null) return null;
         return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
     }
  
     public static LocalDate asLocalDate(Date date) {
+    	if (date==null) return null;
         return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDate();
     }
  
     public static LocalDateTime asLocalDateTime(Date date) {
+    	if (date==null) return null;
         return Instant.ofEpochMilli(date.getTime()).atZone(ZoneId.systemDefault()).toLocalDateTime();
     }
     
